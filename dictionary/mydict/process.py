7e2a8aa7d603d95024685677e8aac401d2c2ee50
Visar Zejnullahu <visar.zejnullahu@gmail.com>
2018-03-20 17:18:36 +0100

    Handle similar inputs

--- dictionary/mydict/process.py
+++ dictionary/mydict/process.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+from difflib import get_close_matches
+
 
 class MyDict(object):
     dictionary = dict()
@@ -18,13 +20,32 @@ class MyDict(object):
     def find_word(cls, word, filename="data.json"):
         if not cls.dictionary:
             cls.read_file()
-        return cls.dictionary[word]
+        word = word.lower()
+        if word in cls.dictionary:
+            return cls.dictionary[word]
+        elif len(get_close_matches(word, cls.dictionary.keys())) > 0:
+            anwser = input(
+                "Did you mean {} instead? Enter Y if yes, or N if no: ".format(
+                    get_close_matches(word, cls.dictionary.keys())[0]))
+            if anwser in ['Y', 'y']:
+                return cls.dictionary[
+                    get_close_matches(word, cls.dictionary.keys())[0]]
+            elif anwser in ['N', 'n']:
+                return "The word doesn't exist"
+            else:
+                return "Please anwser Y or N"
+        else:
+            return "The word doesn't exist"
 
 
 def main():
     word = input("Enter word: ")
-    for item in MyDict.find_word(word):
-        print(item)
+    output = MyDict.find_word(word)
+    if type(output) == list:
+        for item in output:
+            print(item)
+    else:
+        print(output)
 
 
 if __name__ == '__main__':

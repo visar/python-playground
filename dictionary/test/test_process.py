416aeddd1fc152c7a567162150605f0aa350a13d
Visar Zejnullahu <visar.zejnullahu@gmail.com>
2018-03-20 22:09:06 +0100

    Add more tests

--- dictionary/test/test_process.py
+++ dictionary/test/test_process.py
@@ -1,5 +1,5 @@
 import unittest
-
+from unittest import mock
 from mydict import MyDict
 
 
@@ -10,6 +10,20 @@ class TestProcess(unittest.TestCase):
     def test_apple(self):
         apples = MyDict.find_word("apple")
         self.assertIn("The wood of the apple tree.", apples)
+
+    @mock.patch('builtins.input', return_value='Y')
+    def test_apples(self, input):
+        apples = MyDict.find_word("apples")
+        self.assertIn("The wood of the apple tree.", apples)
+
+    @mock.patch('builtins.input', return_value='N')
+    def test_nonexisting_apples(self, input):
+        apples = MyDict.find_word("apples")
+        self.assertEqual("The word doesn't exist", apples)
+
+    def test_nonexisting_word(self):
+        match = MyDict.find_word("asdsadasdsadadadasd")
+        self.assertEqual("The word doesn't exist", match)
 
 
 if __name__ == '__name__':
